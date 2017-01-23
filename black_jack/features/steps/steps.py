ec6fd61c49571018f5361c26b53726171596bc75
Vasyl Yablonskyy <vyablonskyy@lohika.com>
2017-01-23 17:31:14 +0200

    add "given" step add comments refactor twentyone.py name because of the typo in it's name

---
+++ black_jack/features/steps/steps.py
@@ -0,0 +1,17 @@
+# The steps in Behave are the link between the descriptive tests in .feature files and actual application code
+
+from behave import *
+from twentyone import *
+
+# Behave steps use annotation that match the names of the phases
+
+@given('a dealer')
+def step_impl(context):
+    context.dealer = Dealer()
+
+# It's important to notice that the text inside of the annotation matches the scenario text exactly.
+# If it doesn't match, the test cannot run
+
+# The context object is passed from step to step, and it is where we can store information to used by other steps.
+# Since this step is a "given", we need to initialize our state.
+# We do that by creating a Dealer object, and attaching that object to the context.
