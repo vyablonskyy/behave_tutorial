5042540541fe23649b5e605b7ff9efcea376a667
Vasyl Yablonskyy <vyablonskyy@lohika.com>
2017-01-23 18:12:26 +0200

    add defs for Dealer and _next_card add more comments

--- black_jack/twentyone.py
+++ black_jack/twentyone.py
@@ -1,2 +1,16 @@
+import random
+
+_cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+
+def _next_card():
+    return random.choice(_cards)
+
+
 class Dealer():
-    pass
+    def __init__(self):
+        self.hand = []
+
+    def new_round(self):
+        self.hand = [_next_card(), _next_card()]  # the _next_card() function will be defined as a top-level function
+                                                  # of the module, along with a definition of the cards
